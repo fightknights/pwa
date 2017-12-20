9c99ccc9f49813d48dc04309d12476f0d757cff7
david kuiken <dhkkuik@gmail.com>
2017-12-20 10:49:56 +0100

    added routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { AppComponent } from './app.component';
+import { AppRouting } from './app.routing';
 
 import { environment } from '../environments/environment';
 import { ListenComponent } from './listen/listen.component';
+import { HomeComponent } from './pages/home/home.component';
+import { Page1Component } from './pages/page1/page1.component';
+import { MenuComponent } from './menu/menu.component';
+import { Page2Component } from './pages/page2/page2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListenComponent
+    ListenComponent,
+    HomeComponent,
+    Page1Component,
+    MenuComponent,
+    Page2Component
   ],
   imports: [
     BrowserModule,
+    AppRouting,
     ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
---
+++ src/app/app.routing.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { HomeComponent } from './pages/home/home.component';
+import { Page1Component } from './pages/page1/page1.component';
+import { Page2Component } from './pages/page2/page2.component';
+
+const routes: Routes = [
+  { path: 'page2', component: Page2Component },
+  { path: 'page1', component: Page1Component },
+  { path: '**', component: HomeComponent },
+  { path: '', component: HomeComponent }
+];
+
+@NgModule ({
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
+})
+
+export class AppRouting {}
