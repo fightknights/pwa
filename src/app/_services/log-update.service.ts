e8699719e8e2dd0efbb5f1d9ad35ab3267fc6cee
david kuiken <dhkkuik@gmail.com>
2017-12-21 10:13:19 +0100

    service

--- src/app/_services/log-update.service.ts
+++ src/app/_services/log-update.service.ts
@@ -4,14 +4,13 @@ import { SwUpdate } from '@angular/service-worker';
 @Injectable()
 export class LogUpdateService {
 
-  constructor(updates: SwUpdate) {
-    updates.available.subscribe(event => {
-      console.log('current version is', event.current);
-      console.log('available version is', event.available);
-    });
-    updates.activated.subscribe(event => {
-      console.log('old version was', event.previous);
-      console.log('new version is', event.current);
-    });
+  constructor(private updates: SwUpdate) { }
+
+  checkAvailable() {
+    return this.updates.available;
+  }
+
+  checkActivated() {
+    return this.updates.activated;
   }
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LogUpdateService } from '../../_services/log-update.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private logUpdate: LogUpdateService) { }
 
   ngOnInit() {
+    this.logUpdate.checkAvailable().subscribe(event => {
+      console.log('current version is', event.current);
+      console.log('available version is', event.available);
+    });
+
+    this.logUpdate.checkActivated().subscribe(event => {
+      console.log('old version was', event.previous);
+      console.log('new version is', event.current);
+    });
   }
 
 }
