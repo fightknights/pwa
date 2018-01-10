49711776a1c7de5ce81c1e1952b36e74b7d905de
david kuiken <dhkkuik@gmail.com>
2018-01-10 15:36:42 +0100

    new push keys

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,6 +6,6 @@
 export const environment = {
   production: false,
 
-  push_public: 'BPfFWto97Cu5PclNLkE7pGmnk3KliGPsWc_UujqMjKvCYwqaHWIUDy6rOTEH8M-vpwUNJO9-eVaBZDywjOrgn1c',
-  push_private: 'hqwi2kXWDbPaz5cHvln9DiDN46oJjCyxaxAN4eZ_W3Y'
+  push_public: 'BNSfGfC55IXBVe3zs_3D_z-V_JaY_93AIDyfBztpOPzkp8nHzJ1T933WGEmkNsoByYdgVaiLMnRzex5ui_oDGpU',
+  push_private: '_ppJOEOOS8bvUCSB4op5nwi4RgsuUJ1fvz_RWmTiYkU'
 };
