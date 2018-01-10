fece1f99dcb7a35b39a919a838da224a5f810ce3
david kuiken <dhkkuik@gmail.com>
2018-01-10 10:53:08 +0100

    push check

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { LogUpdateService } from '../../_services/log-update.service';
+import { SwPush } from '@angular/service-worker';
 
 @Component({
   selector: 'app-home',
@@ -8,9 +9,17 @@ import { LogUpdateService } from '../../_services/log-update.service';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private logUpdate: LogUpdateService) { }
+  push_enabled: string;
+
+  constructor(
+    private logUpdate: LogUpdateService,
+    private _push: SwPush
+  ) { }
 
   ngOnInit() {
+
+    this.push_enabled = ' ';
+
     this.logUpdate.checkAvailable().subscribe(event => {
       console.log('current version is', event.current);
       console.log('available version is', event.available);
@@ -20,6 +29,14 @@ export class HomeComponent implements OnInit {
       console.log('old version was', event.previous);
       console.log('new version is', event.current);
     });
+  }
+
+  subscribe() {
+    if (this._push.isEnabled) {
+      this.push_enabled = 'True';
+    } else {
+      this.push_enabled = 'False';
+    }
   }
 
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+
+  push_public: 'BPfFWto97Cu5PclNLkE7pGmnk3KliGPsWc_UujqMjKvCYwqaHWIUDy6rOTEH8M-vpwUNJO9-eVaBZDywjOrgn1c',
+  push_private: 'hqwi2kXWDbPaz5cHvln9DiDN46oJjCyxaxAN4eZ_W3Y'
 };
